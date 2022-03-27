8dbd38ac561f70c319d2ed27a29643b4076627e2
joebed <joebed@umich.edu>
2022-03-27 19:10:25 -0400

    removed specials.py

--- specials.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-LOGGER = logging.getLogger('discord')
-LOGGER.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-LOGGER.addHandler(handler)
-
-TOKEN = "OTQ1MTI0MzExMDg0MTgzNTcz.YhLlmw.P2kEwJJNocflaXrFNnOMz4TNxA4"
